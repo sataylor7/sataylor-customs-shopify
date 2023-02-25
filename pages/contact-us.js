b47d54fa2833692b61481b58a7e7ddcea8bcb93f
SATaylor <engelstern7@yahoo.com>
2023-02-25 14:29:56 -0600

    testing my deployment

---
+++ pages/contact-us.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ContactUs() {
+  return (
+    <div>
+      <div>
+        <div className='bg-slate-100'>
+          <h1 className='text-3xl max-w-2xl mx-auto px-4 py-8 sm:px-6 lg:max-w-7xl lg:px-8'>
+            Contact Us
+          </h1>
+        </div>
+        <div className='max-w-2xl mx-auto px-4 py-8 sm:px-6 lg:max-w-7xl lg:px-8 gap-y-3 flex flex-col'></div>
+      </div>
+    </div>
+  );
+}
