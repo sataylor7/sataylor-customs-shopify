725dbf40b04e8fadffe09d24e662a08eed9456b4
SATaylor <engelstern7@yahoo.com>
2023-03-01 17:42:46 -0600

    switching to isr

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,38 +1,42 @@
-import ProductPageContent from "../../components/ProductPageContent"
-import { getAllProducts, getProduct, recursiveCatalog } from "../../lib/shopify"
+import ProductPageContent from '../../components/ProductPageContent';
+import {
+  getAllProducts,
+  getProduct,
+  recursiveCatalog,
+} from '../../lib/shopify';
 
 export default function ProductPage({ product }) {
-
   return (
-    <div className="min-h-screen py-12 sm:pt-20">
+    <div className='min-h-screen py-12 sm:pt-20'>
       <ProductPageContent product={product} />
     </div>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const products = await recursiveCatalog()
+  const products = await recursiveCatalog();
 
-  const paths = products.map(item => {
-    const product = String(item.node.handle)
+  const paths = products.map((item) => {
+    const product = String(item.node.handle);
 
     return {
-      params: { product }
-    }
-  })
+      params: { product },
+    };
+  });
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: blocking,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const product = await getProduct(params.product)
+  const product = await getProduct(params.product);
 
   return {
     props: {
-      product
-    }
-  }
+      product,
+    },
+    revalidate: 60,
+  };
 }
