40c0d7b19ba2943e4544bb7c2bce7b2a5530566f
SATaylor <engelstern7@yahoo.com>
2023-03-09 20:01:38 -0600

    adding the old shop context for reference

---
+++ context/shopContext-old.js
@@ -0,0 +1,181 @@
+import { createContext, useState, useEffect } from 'react';
+import {
+  createCheckout,
+  updateCheckout,
+  checkoutCustomerAssociateV2,
+  fetchCheckout,
+} from '../lib/shopify';
+
+const CartContext = createContext();
+
+export default function ShopProvider({ children }) {
+  const [cart, setCart] = useState([]);
+  const [cartOpen, setCartOpen] = useState(false);
+  const [checkoutId, setCheckoutId] = useState('');
+  const [checkoutUrl, setCheckoutUrl] = useState('');
+  const [cartLoading, setCartLoading] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.checkout_id) {
+      const cartObject = JSON.parse(localStorage.checkout_id);
+
+      if (cartObject[0].id) {
+        setCart([cartObject[0]]);
+      } else if (cartObject[0].length > 0) {
+        setCart(...[cartObject[0]]);
+      }
+
+      setCheckoutId(cartObject[1].id);
+      setCheckoutUrl(cartObject[1].webUrl);
+    }
+  }, []);
+
+  async function addToCart(addedItem) {
+    const newItem = { ...addedItem };
+    console.log(newItem);
+    setCartOpen(true);
+
+    if (cart.length === 0) {
+      setCart([newItem]);
+
+      const checkout = await createCheckout(newItem.id, 1);
+      setCheckoutId(checkout.id);
+      setCheckoutUrl(checkout.webUrl);
+
+      localStorage.setItem('checkout_id', JSON.stringify([newItem, checkout]));
+    } else {
+      let newCart = [];
+      let added = false;
+
+      cart.map((item) => {
+        if (item.id === newItem.id) {
+          item.variantQuantity++;
+          newCart = [...cart];
+          added = true;
+        }
+      });
+
+      if (!added) {
+        newCart = [...cart, newItem];
+      }
+
+      setCart(newCart);
+      const newCheckout = await updateCheckout(checkoutId, newCart);
+      localStorage.setItem(
+        'checkout_id',
+        JSON.stringify([newCart, newCheckout])
+      );
+    }
+  }
+
+  async function removeCartItem(itemToRemove) {
+    const updatedCart = cart.filter((item) => item.id !== itemToRemove);
+    setCartLoading(true);
+
+    setCart(updatedCart);
+
+    const newCheckout = await updateCheckout(checkoutId, updatedCart);
+
+    localStorage.setItem(
+      'checkout_id',
+      JSON.stringify([updatedCart, newCheckout])
+    );
+    setCartLoading(false);
+
+    if (cart.length === 1) {
+      setCartOpen(false);
+    }
+  }
+
+  async function incrementCartItem(item) {
+    setCartLoading(true);
+
+    let newCart = [];
+
+    cart.map((cartItem) => {
+      if (cartItem.id === item.id) {
+        cartItem.variantQuantity++;
+        newCart = [...cart];
+      }
+    });
+    setCart(newCart);
+    const newCheckout = await updateCheckout(checkoutId, newCart);
+
+    localStorage.setItem('checkout_id', JSON.stringify([newCart, newCheckout]));
+    setCartLoading(false);
+  }
+
+  async function decrementCartItem(item) {
+    setCartLoading(true);
+
+    if (item.variantQuantity === 1) {
+      removeCartItem(item.id);
+    } else {
+      let newCart = [];
+      cart.map((cartItem) => {
+        if (cartItem.id === item.id) {
+          cartItem.variantQuantity--;
+          newCart = [...cart];
+        }
+      });
+
+      setCart(newCart);
+      const newCheckout = await updateCheckout(checkoutId, newCart);
+
+      localStorage.setItem(
+        'checkout_id',
+        JSON.stringify([newCart, newCheckout])
+      );
+    }
+    setCartLoading(false);
+  }
+
+  async function clearCart() {
+    const updatedCart = [];
+
+    setCart(updatedCart);
+
+    const newCheckout = await updateCheckout(checkoutId, updatedCart);
+
+    localStorage.setItem(
+      'checkout_id',
+      JSON.stringify([updatedCart, newCheckout])
+    );
+  }
+
+  async function associateCartToCustomer(token) {
+    const updatedCheckout = await checkoutCustomerAssociateV2(
+      checkoutId,
+      token
+    );
+    setCheckoutUrl(updatedCheckout.webUrl);
+    console.log(updatedCheckout);
+    localStorage.setItem(
+      'checkout_id',
+      JSON.stringify([cart, updatedCheckout])
+    );
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        cartOpen,
+        setCartOpen,
+        addToCart,
+        checkoutUrl,
+        removeCartItem,
+        clearCart,
+        cartLoading,
+        incrementCartItem,
+        decrementCartItem,
+        associateCartToCustomer,
+      }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+const ShopConsumer = CartContext.Consumer;
+
+export { ShopConsumer, CartContext };
