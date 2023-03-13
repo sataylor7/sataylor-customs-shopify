12953b6d739273243ed5b7a0a856468e691226bb
SATaylor <engelstern7@yahoo.com>
2023-03-13 15:38:49 -0500

    getting the recommended list ready to pass in the title

--- components/RecommendedList.js
+++ components/RecommendedList.js
@@ -1,12 +1,14 @@
 import ProductCard from './ProductCard';
 
-const RecommendedList = ({ products, current }) => {
+const RecommendedList = ({
+  products,
+  current,
+  title = 'Recommended Products',
+}) => {
   return (
     <div className='bg-white'>
       <div className='max-w-2xl mx-auto md:py-16 md:pt-2 md:px-4 py-24 px-6 lg:max-w-7xl lg:px-8'>
-        <h2 className='text-2xl font-extrabold text-gray-900 mb-6'>
-          Recommended Products
-        </h2>
+        <h2 className='text-2xl font-extrabold text-gray-900 mb-6'>{title}</h2>
         <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
           {products.map((product) =>
             product.node.id === current ? null : (
